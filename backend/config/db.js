98968c282df0198a30abdc58f5bb498ed8d13cdc
thedhruvish <thedhruvish@gmail.com>
2026-02-25 14:48:33 +0530

    fix: change the db connectios

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,21 @@
 import mongoose from "mongoose";
 
-let conn = null;
+let isConnected = false;
+
 export const connectDB = async () => {
+  if (isConnected) return;
+
   const mongodbURL = process.env.MONGO_URL;
   if (!mongodbURL) {
     throw new Error("add Mongodb Url in env file");
   }
-  if (conn) return conn;
-  if (process.env.AWS_LAMBDA_FUNCTION_NAME) {
-    conn = mongoose.createConnection(mongodbURL, {
-      serverSelectionTimeoutMS: 5000,
-    });
-    await conn.asPromise();
-  } else {
-    await mongoose.connect(mongodbURL, {
-      tls: true,
-      tlsAllowInvalidCertificates: true,
-    });
-  }
+
+  mongoose.set("bufferCommands", false);
+
+  await mongoose.connect(mongodbURL, {
+    serverSelectionTimeoutMS: 5000,
+  });
+
+  isConnected = true;
+  console.log("MongoDB connected");
 };
